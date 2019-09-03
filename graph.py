389a154fd6d92266268722d8e4be27d43b295e59
dudami43 <dudami43@gmail.com>
2019-09-02 21:58:09 -0300

    presentation

--- graph.py
+++ graph.py
@@ -8,27 +8,28 @@ os.environ["PATH"] += os.pathsep + 'C:\\graphviz\\bin'
 def main():
 
     files = glob.glob("dados/*.json")
+    quotes_trump = glob.glob("dados/replies_238717783007977473_*.json")
     g = Graph('G')
 
-    # for each in files:
+    for each in quotes_trump:
 
-    try:
-        with open("dados/replies_238717783007977473.json", 'r', encoding="utf8") as f:
-            data = json.load(f)
+        try:
+            with open(each, 'r', encoding="utf8") as f:
+                data = json.load(f)
 
-        g.attr('node')
+            g.attr('node')
 
-        for key in data:
-            g.node(key)
+            for key in data:
+                g.node(key)
 
-        for key in data:
-            if(key is not None):
-                if(data[key]["replie_to"] is not None):
-                    g.edge(key, data[key]["replie_to"])
-                # if(data[key]["quoting"] is not None):
-                    # g.edge(key, data[key]["quoting"], color="red")
-    except:
-        print("each")
+            for key in data:
+                if(key is not None):
+                    if(data[key]["replie_to"] is not None):
+                        g.edge(key, data[key]["replie_to"])
+                    # if(data[key]["quoting"] is not None):
+                        # g.edge(key, data[key]["quoting"], color="red")
+        except:
+            print("each")
 
     g.view()
 

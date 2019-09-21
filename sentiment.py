036a8ae6ff79aeac962ee991015724158679e8c9
dudami43 <dudami43@gmail.com>
2019-09-21 17:48:55 -0300

    arvores mais positivas

--- sentiment.py
+++ sentiment.py
@@ -4,18 +4,21 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 analyser = SentimentIntensityAnalyzer()
 
+
 def sentiment_analyzer_scores(sentence):
     score = analyser.polarity_scores(sentence)
     return score
-files = glob.glob("dados/*.json")
+
+
+files = glob.glob("dados\\believes\\*.json")
 for filename in files:
-    with open(filename, 'r', encoding="utf8") as f:  
-        print(filename)  
+    with open(filename, 'r', encoding="utf8") as f:
+        print(filename)
         data = json.load(f)
 
         for item in data:
             score = sentiment_analyzer_scores(data[item]["text"])
             data[item]['sentiment'] = score['compound']
- 
+
     with open(filename, 'w', encoding="utf8") as f:
         f.write(json.dumps(data, ensure_ascii=False))
